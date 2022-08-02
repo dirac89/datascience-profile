e252b1c5f5a83c6b1a01826edfc3bfa431da611e
dirac89 <aguilerajavier58@gmail.com>
2022-08-02 12:15:49 +0200

    Fix personal info

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,14 @@ col2.image(Image.open('dp.png'))
 
 st.header('Javier Aguilera Fernandez, Ph.D.')
 
-st.info('Ph.D. in Physics with interest in Data Science and Deep Learning)
+st.info('Ph.D. in Physics with interest in Data Science and Deep Learning')
 
 icon_size = 20
 
 #st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
 #st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
 #st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/Javier1791', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'www.linkedin.com/in/javier-aguilera-fernández-295a478a', 'Follow me on LinkedIn', icon_size)
+st_button('twitter', '//twitter.com/Javier1791', 'Follow me on Twitter', icon_size)
+st_button('linkedin', '//www.linkedin.com/in/javier-aguilera-fernández-295a478a', 'Follow me on LinkedIn', icon_size)
 #st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
 #st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
